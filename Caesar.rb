ee0e135d262e9a285f662092c95a3c5a8fd62530
Viaka <rharper9@gmail.com>
2017-11-28 16:50:42 -0600

    Upper Case first half of alpha working

--- Caesar.rb
+++ Caesar.rb
@@ -1,11 +1,20 @@
 def shift(phrase, val)
   phrase.split("").each { |x| 
-    if x.ord > 64 and x.ord < 90 {}
+    #checks if the value after shift is an upper case letter
+    if ((x.ord + val) > 64 and (x.ord + val) < 91) then
+    #Checks if the value after shift is out of bounds
+      if (x.ord + val) > 90 then puts (x.ord + val - 26).chr
+      else puts (x.ord + val).chr
+      end
+    end
     
-    + val).chr
+    #elsif ((x.ord - val) > 96 and (x.ord - val) < 123) then puts (x.ord + val).chr
+    
+    #puts x.chr
+   # + val).chr
     
   }
-  puts final
+  #puts final
 end
 
-shift "Hello", 3
+shift "WXYZ", 3
